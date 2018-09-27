9f34f9d3cf96e273cc77d05d6c0e25edc0556385
Leporoni <leporoni@hotmail.com>
2018-09-27 13:42:46 -0300

    first commit

--- cliente_repository.py
+++ cliente_repository.py
@@ -2,6 +2,7 @@ import MySQLdb
 
 class ClienteRepository():
 
+    @staticmethod
     def listar_clientes():
         db = MySQLdb.connect(user="root", passwd="123456", db="treinaweb_clientes", host="localhost", autocommit=True)
         cursor = db.cursor()
@@ -9,12 +10,14 @@ class ClienteRepository():
         print(cursor.fetchall())
         db.close()
 
+    @staticmethod
     def inserir_cliente(cliente):
         db = MySQLdb.connect(user="root", passwd="123456", db="treinaweb_clientes", host="localhost", autocommit=True)
         cursor = db.cursor()
         cursor.execute("INSERT INTO cliente (nome, idade) values(%s, %s)", (cliente.nome, cliente.idade))
         db.close()
 
+    @staticmethod
     def editar_cliente(id, cliente):
         db = MySQLdb.connect(user="root", passwd="123456", db="treinaweb_clientes", host="localhost", autocommit=True)
         cursor = db.cursor()
@@ -22,6 +25,7 @@ class ClienteRepository():
                        ({'nome': cliente.nome, 'idade': cliente.idade, 'id': id}))
         db.close()
 
+    @staticmethod
     def remover_cliente(id):
         db = MySQLdb.connect(user="root", passwd="123456", db="treinaweb_clientes", host="localhost", autocommit=True)
         cursor = db.cursor()
